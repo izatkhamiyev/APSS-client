cea908e14f03c36e80d00a5ad0c49ec70e81c0bd
izatkhamiyev <izat0011@gmail.com>
2019-02-14 21:51:28 +0600

    nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, Button} from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, Button } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
@@ -22,37 +22,39 @@ class Header extends Component {
 
     render() {
         return (
-            <Navbar dark expand='md'>
-                <div className='container'>
-                    <NavbarBrand className='mr-auto col-2' href='/ '>APSS</NavbarBrand>
-                    <NavbarToggler onClick={this.toggle} />
-                    <Collapse className="" isOpen={this.state.isNavOpen} navbar>
-                        <Nav style={{ flex: 1 }} navbar>
-                            <NavItem className="nav-item">
-                                <NavLink className='nav-link' to='/home'>Home</NavLink>
-                            </NavItem>
-                            <NavItem className="nav-item">
-                                <NavLink className='nav-link' to='/articles'>Articles</NavLink>
-                            </NavItem>
-                            <NavItem className="nav-item">
-                                <NavLink className='nav-link' to='/contact'>Contact us</NavLink>
-                            </NavItem>
-                            <NavItem className="nav-item">
-                                <NavLink className='nav-link' to='/about'>About</NavLink>
-                            </NavItem>
-                            {
-                                this.props.logoutPage ?
-                                <NavItem className="nav-item ml-auto">
-                                    <Button outline color="primary" className='nav-link' onClick={this.props.logoutAdmin}>Logout</Button>
+            <header className="header">
+                <Navbar dark expand='md'>
+                    <div className='container'>
+                        <NavbarBrand className='' href='/ '>APSS</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav style={{ flex: 1 }} navbar className="col-12 col-md-8 justify-content-center">
+                                <NavItem className="nav-item">
+                                    <NavLink className='nav-link' to='/home'>Home</NavLink>
                                 </NavItem>
-                                :
-                                null
+                                <NavItem className="nav-item">
+                                    <NavLink className='nav-link' to='/articles'>Articles</NavLink>
+                                </NavItem>
+                                <NavItem className="nav-item">
+                                    <NavLink className='nav-link' to='/contact'>Contact us</NavLink>
+                                </NavItem>
+                                <NavItem className="nav-item">
+                                    <NavLink className='nav-link' to='/about'>About</NavLink>
+                                </NavItem>
+                                {
+                                    this.props.logoutPage ?
+                                        <NavItem className="nav-item ml-auto">
+                                            <Button outline color="primary" className='nav-link' onClick={this.props.logoutAdmin}>Logout</Button>
+                                        </NavItem>
+                                        :
+                                        null
 
-                            }
-                        </Nav>
-                    </Collapse>
-                </div>
-            </Navbar>
+                                }
+                            </Nav>
+                        </Collapse>
+                    </div>
+                </Navbar>
+            </header>
         );
     }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,52 @@
 import React, { Component } from 'react';
 import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
+
+const RenderItem = ({ article }) => {
+    return (
+        <div>
+            <Card className='col-12'>
+                <Link className="link-article" to={`/articles/${article._id}`} style={{ textDecoration: 'none' }}>
+                    <CardImg top src={article.image} alt={article.title} />
+                    <CardBody>
+                        <CardTitle><h4>{article.title}</h4></CardTitle>
+                        {article.subtitle ? <CardSubtitle>{article.subtitle}</CardSubtitle> : null}
+                        <CardText>{article.description}</CardText>
+                    </CardBody>
+                </Link>
+            </Card>
+        </div>
+    );
+}
 class Home extends Component {
 
 
     render() {
         const renderArticles = this.props.articlesInfo.articles.map((article) => {
-            return <div key={article._id}> {article.title} </div>
+            return <RenderItem article={article} />
         });
 
         return (
             <div>
-                <div id='main'>
-                    <Header />
-                    <div className="h-100">
-                        <div className="col-12 offset-md-2 col-md-8">
-                            {renderArticles}
-                        </div>
+                <Header />
+                <div id="main">
+                    <div>
+                        <h1>See our new articles</h1>
+                        <Link to='/articles' className='btn'>Articles</Link>
                     </div>
-                    askdfj;
-                    {/* <Footer /> */}
                 </div>
-
-
+                <div className='container mt-3 mb-3'>
+                    <div className="col-12 col-md-4">
+                        {renderArticles}
+                    </div>
+                </div>
+                <Footer />
             </div >
         );
     }
